13f36e1d907326fef81ed31d61347538604356a7
ziad501 <ziadmuhammed501@gmail.com>
2026-09-12 10:51:25 +0300

    feat: implement user list with dynamic data binding and update user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,7 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,16 +11,24 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUsers = signal(DUMMY_USERS[randomIndex]);
+  @Input() avatar!: string;
+  @Input() name!: string;
 
-  imagePath = computed(() => 'assets/users/' + this.selectedUsers().avatar);
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
+  onSelectUser() {}
+  // selectedUsers = signal(DUMMY_USERS[randomIndex]);
+
+  // imagePath = computed(() => 'assets/users/' + this.selectedUsers().avatar);
 
   // get imagePath() {
   //   return 'assets/users/' + this.selectedUsers.avatar;
   // }
 
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUsers.set(DUMMY_USERS[randomIndex]);
-  }
+  // onSelectUser() {
+  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  //   this.selectedUsers.set(DUMMY_USERS[randomIndex]);
+  // }
 }
